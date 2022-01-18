041af84dc44478697625f4208d034413756af0a8
Alexander Mann <amann@c1r5p6.hive.fi>
2022-01-18 16:06:37 +0200

    more playing with printf

--- main.c
+++ main.c
@@ -6,12 +6,13 @@
 /*   By: amann <amann@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 12:42:19 by amann             #+#    #+#             */
-/*   Updated: 2022/01/18 12:48:05 by amann            ###   ########.fr       */
+/*   Updated: 2022/01/18 14:36:23 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int	add_numbers(int n, ...)
 {
@@ -25,8 +26,21 @@ int	add_numbers(int n, ...)
 	return (sum);
 }
 
+int	ft_printf(char *s, ...)
+{
+	//va_list ptr;
+	//va_start(ptr, s);
+	for (int i = 0; s[i] != '\0'; i++)
+		write(1, &s[i], 1);
+	//va_end(ptr);
+	return (0);
+}
+
 int main(void)
 {
+	printf("this is a width test%10d!\n", 123);
+	printf("this is a precision & width test %10.5d\n", 123);
 	printf("\n3 + 4 + 5 + 6 = %d\n", add_numbers(4, 3, 4, 5, 6));
+	ft_printf("hello world\n");
 	return (0);
 }
