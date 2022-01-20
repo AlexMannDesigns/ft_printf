def7a84690a92291ef0f412bd658866f7ecac4f0
AlexMannDesigns <alex.mann1989@gmail.com>
2022-01-20 13:24:07 +0200

    added separate function to handle char inputs

--- conversion_control.c
+++ conversion_control.c
@@ -17,20 +17,27 @@
 *	ft_is... functions might be needed for error handling
 *	p & f conversions still needed
 */
+static char	*handle_char(char c)
+{
+	char *res;
+
+	res = ft_strnew(2);
+	res[0] = c;
+	return (res);
+}
+
 char	*conversion_control(char *s, va_list lst, t_flags flag_data)
 {
 	char	*res;
 
 	res = NULL;
-	if (s[0] == 'd' || s[0] == 'i' || s[0] == 'o' || s[0] == 'u' || s[0] == 'x' || s[0] == 'X')
+	if (s[0] == 'd' || s[0] == 'i' || s[0] == 'o' || s[0] == 'u'
+		|| s[0] == 'x' || s[0] == 'X')
 		numeric_conv_dispatcher(s[0], va_arg(lst, int), &res);
 	else if (s[0] == 's')
 		res = ft_strdup(va_arg(lst, char *));
 	else if (s[0] == 'c')
-	{
-		res = ft_strnew(2);
-		res[0] = (char) va_arg(lst, int);
-	}
+		res = handle_char((char) va_arg(lst, int));
 	else
 		res = ft_strdup("*** I'm not entirely sure this is the conversion you meant! ***"); //just here for testing
 	return (res);
